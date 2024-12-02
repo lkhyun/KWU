c85b82ff3c66445214229cd99567af1a56b355f1
lkhyun <dlrkdgus999@gmail.com>
2024-12-02 16:52:49 +0900

    upload

--- Proj1_2019202050/Proj1_2019202050/src/main/java/com/example/Proj1_2019202050/domain/repository/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Proj1_2019202050.domain.repository;
-
-import com.example.Proj1_2019202050.domain.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long>//JpaRepository 사용
-{
-}
